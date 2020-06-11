658259962906832561778c300bca403344be0f5f
Vikas Chandra Thakur <62649975+vcthakur@users.noreply.github.com>
2020-06-11 12:43:26 +0530

    Add files via upload

---
+++ server.py
@@ -0,0 +1,34 @@
+from copyreg import pickle
+
+from flask import Flask, request, jsonify
+import util
+
+app = Flask(__name__)
+
+@app.route('/get_loc_name', methods=['GET'])
+def get_loc_name():
+    response = jsonify({
+        'locations': util.get_loc_name()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
+def predict_home_price():
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
+
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
+
+if __name__ == "__main__":
+    print("Starting Python Flask Server For Home Price Prediction...")
+    util.load_saved_artifacts()
+    app.run()
